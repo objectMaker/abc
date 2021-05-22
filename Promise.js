8e0e26f5b0c785382bb798f7c99a47d6090b59e0
objectMaker <528154320@qq.com>
2021-05-22 20:12:09 +0800

    初始化文件

---
+++ Promise.js
@@ -0,0 +1,21 @@
+function Promise(){
+
+}
+Promise.prototype.then=function (){
+
+}
+Promise.prototype.catch = function (){
+
+}
+Promise.resolve = function (){
+
+};
+Promise.reject = function (){
+
+}
+Promise.all = function (){
+
+}
+Promise.race = function (){
+
+}
